6373aa9a8b20cc57c8d37b328b3b534ac68a4de4
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-01-04 12:00:24 +0100

    implemented useEffect to store login info, so it will be persistent after page reload.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    //it will only run if the dependecies are changed, and the component is reevaulated
+    const storedUserLogInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLogInfo === "1") {
+      setIsLoggedIn(true); //this triggers the component fuction to run again
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1"); //this stores some info in the browsers local storage
+    // so we can save data, and load back in case of page reload
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
